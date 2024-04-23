06e969da00fc05c4113077f21430983e95bcb0ac
Johnny Ramsay <johnnyramsay@Johnnys-Air.lan>
2024-04-23 19:41:16 +0100

    create Form, list and stats compontents and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Navbar from "./components/Navbar";
+import Form from "./components/Form";
+import List from "./components/List";
+import Stats from "./components/Stats";
 
 function App() {
   return (
-    <div>
-      <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
-
+    <div className="w-full h-screen grid grid-rows-[200px_auto_1fr_auto]">
       <Navbar />
+      <Form />
+      <List />
+      <Stats />
     </div>
   );
 }

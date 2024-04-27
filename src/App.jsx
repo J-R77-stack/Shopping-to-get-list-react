a79b6d0e0317f0b230148035cdeef720f681856c
Johnny Ramsay <johnnyramsay@Johnnys-Air.lan>
2024-04-27 11:23:22 +0100

    create stat information in footer for user

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ export default function App() {
           onToggleItems={HandleToggleItem}
         />
 
-        <Stats />
+        <Stats items={items} />
       </div>
     </>
   );
@@ -130,10 +130,23 @@ function Item({ item, onDeletingItems, onToggleItems }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="bg-emerald-200 text-center font-semibold p-[3.2rem]">
+        Start adding some items to your list ⬆
+      </p>
+    );
+
+  const numItems = items.length;
+  const numDone = items.filter((item) => item.done).length;
+  const percentage = Math.round((numDone / numItems) * 100);
   return (
     <footer className="bg-emerald-200 text-center font-semibold p-[3.2rem]">
-      You have X items on your List
+      {percentage === 100
+        ? "You have completed your list  🎉 👏🏻 🍾 "
+        : `You have ${numItems} items on your List, and you have already completed
+     ${numDone} (${percentage}%)`}
     </footer>
   );
 }
